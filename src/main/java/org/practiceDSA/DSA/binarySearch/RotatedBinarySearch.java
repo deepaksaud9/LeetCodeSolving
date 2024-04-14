645854847acc05b1444f1c1db7457c8bf11527b2
Deepak <77203703+deepaksaud9@users.noreply.github.com>
2024-04-14 18:11:18 +0545

    rotated Binary Search with target value

--- src/main/java/org/practiceDSA/DSA/binarySearch/RotatedBinarySearch.java
+++ src/main/java/org/practiceDSA/DSA/binarySearch/RotatedBinarySearch.java
@@ -5,8 +5,25 @@ public class RotatedBinarySearch  {
     public static void main(String[] args) {
 
         int[] arr = {4,5,6,7,0,1,2};
-        System.out.println(findPivot(arr));
+        int target = 6;
+        System.out.println(search(arr, target));
 
+    }
+
+    static  int search(int[] nums, int target){
+        int pivot  = findPivot(nums);
+        //if we did not find the pivot, it means the array is not rotated
+        if (pivot == -1){
+            return binarySearch(nums, target, 0, nums.length-1);
+        }
+        //if pivot is found, we have to found 2 asc sorted array
+        if (nums[pivot] == target){
+            return pivot;
+        } if (target >= nums[0]){
+            return binarySearch(nums, target, 0, pivot - 1);
+        }
+
+        return binarySearch(nums, target, pivot + 1, nums.length - 1);
     }
 
     static int findPivot(int[] arr){
@@ -24,6 +41,27 @@ public class RotatedBinarySearch  {
                 end = mid -1;
             }else {
                 start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end){
+
+        while (start <= end){
+            //find middle element
+            //    int mid = (start + end)/2;   //might be possible (start + end) / 2 is exceeds then the value of integer; at that time ge got errors.
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]){
+                end = mid - 1;
+            }
+            else if (target > arr[mid]){
+                start = mid + 1;
+
+            }else {
+                //final answer
+                return mid;
             }
         }
         return -1;
