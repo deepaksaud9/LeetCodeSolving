c819876e6f09d60ac37535613ce12b6d7c4a3d8b
Deepak <77203703+deepaksaud9@users.noreply.github.com>
2024-04-17 15:42:24 +0545

    selection sort

--- src/main/java/org/practiceDSA/DSA/BubbleSort/BubbleSort.java
+++ src/main/java/org/practiceDSA/DSA/BubbleSort/BubbleSort.java
@@ -3,12 +3,6 @@ package org.practiceDSA.DSA.BubbleSort;
 import java.util.Arrays;
 
 public class BubbleSort {
-    public static void main(String[] args) {
-
-        int[] arr = {1,2,3,4,5};
-         bubble(arr);
-        System.out.println(Arrays.toString(arr));
-    }
 
     static void bubble(int[] arr ){
         boolean swapped ;
