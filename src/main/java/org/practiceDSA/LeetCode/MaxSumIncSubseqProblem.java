e866f25e2a5a53879877bcb67c47c6d14319dd50
Deepak <77203703+deepaksaud9@users.noreply.github.com>
2024-07-08 23:44:37 +0545

    Maximum Sum Increasing Subsequence Problem : modified

--- src/main/java/org/practiceDSA/LeetCode/MaxSumIncSubseqProblem.java
+++ src/main/java/org/practiceDSA/LeetCode/MaxSumIncSubseqProblem.java
@@ -1,5 +1,15 @@
 package org.practiceDSA.LeetCode;
 
+/*
+    Maximum Sum Increasing Subsequence Problem
+        Find a subsequence of a given sequence such that the subsequence sum is as high as possible and
+        the subsequence’s elements are sorted in ascending order. This subsequence is not necessarily
+        contiguous or unique.
+        Please note that the problem specifically targets subsequences that need not be contiguous, i.e.,
+        subsequences are not required to occupy consecutive positions within the original sequences.
+        For example, consider the subsequences {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11}.
+*/
+
 public class MaxSumIncSubseqProblem {
 
     public static void main(String args[]) {
